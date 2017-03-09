f605931553958c65bcbb03973578ee0b6dd15c17
Erik Barajas <erik.barajas93@gmail.com>
2017-03-08 22:10:53 -0600

    Creacion de formulario de Login con Forms.py

--- admin_facilito/clients/views.py
+++ admin_facilito/clients/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from forms import LoginForm
 
 # Create your views here.
 def show(request):
@@ -7,7 +8,15 @@ def show(request):
 
 
 def login(request):
-    nombre = 'Erik'
-    edad   = 12
-    context = {'nombre' : nombre, 'edad': edad }
+    # Si nos estan enviando el formulario .:.
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        print username
+        print password
+
+    form = LoginForm()
+    context = {
+        'form': form
+    }
     return render(request, 'login.html', context)
