6192476299a3f66ddae118d86a955fb60a1111cc
Erik Barajas <erik.barajas93@gmail.com>
2017-05-03 23:48:31 -0500

    Se crea el Create Project

--- admin_facilito/projects/urls.py
+++ admin_facilito/projects/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 app_name = 'project'
 urlpatterns = [
-
+    url(r'^create/$', views.CreateClass.as_view(), name="create"),
 ]
--- admin_facilito/projects/views.py
+++ admin_facilito/projects/views.py
@@ -3,6 +3,13 @@ from django.shortcuts import render
 from django.views.generic import CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin # Valida si el usuario esta loggeado
 
+from .models import Project
+
+from .forms import ProjectForm
+
 # Create your views here.
 class CreateClass(CreateView, LoginRequiredMixin):
-    pass
+    login_url     = 'client:login'
+    template_name = 'project/create.html'
+    model         = Project
+    form_class    = ProjectForm
