799209bf4b4149a5e472fc36c36004e8efd0e778
Erik Barajas <erik.barajas93@gmail.com>
2017-05-04 23:26:53 -0500

    Creacion del ListView para listar los projectos de un usuario

--- admin_facilito/projects/urls.py
+++ admin_facilito/projects/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'project'
 urlpatterns = [
     url(r'^create/$', views.CreateClass.as_view(), name="create"),
+    url(r'^my_projects/$', views.ListClass.as_view(), name="my_projects"),
 ]
--- admin_facilito/projects/views.py
+++ admin_facilito/projects/views.py
@@ -1,15 +1,31 @@
 from django.shortcuts import render
 
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
 from django.contrib.auth.mixins import LoginRequiredMixin # Valida si el usuario esta loggeado
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse_lazy
 
 from .models import Project
 
 from .forms import ProjectForm
 
 # Create your views here.
-class CreateClass(CreateView, LoginRequiredMixin):
+class CreateClass(LoginRequiredMixin, CreateView):
     login_url     = 'client:login'
+    success_url   = reverse_lazy('client:dashboard')
     template_name = 'project/create.html'
     model         = Project
     form_class    = ProjectForm
+
+    def form_valid(self, form):
+        self.object = form.save(commit = False)
+        self.object.user = self.request.user
+        self.object.save()
+        return HttpResponseRedirect( self.get_success_url() )
+
+class ListClass( LoginRequiredMixin, ListView):
+    login_url     = 'client:login'
+    template_name = 'project/own.html'
+
+    def get_queryset(self):
+        return Project.objects.filter(user = self.request.user).order_by('dead_line')
